4d8e97a51e50876a4c856e53fb141f3e298c57bd
JANKOH INDIA <jankohindia@gmail.com>
2026-06-15 18:23:35 +0530

    Add files via upload

--- assets/scripts/theme.js
+++ assets/scripts/theme.js
@@ -1,47 +1,24 @@
 function updateTheme() {
-    const now = new Date();
-    const hour = now.getHours();
-    const minute = now.getMinutes();
-    const bodyClass = document.body.classList;
-    const sectionClass = document.getElementById('landingSection').classList;
+    const hour = new Date().getHours();
+    const isDayTime = hour >= 6 && hour < 19;
 
-    // Define transition times
-    const dayStartHour = 5;
-    const dayStartMinute = 59;
-    const nightStartHour = 18;
-    const nightStartMinute = 59;
-
-    // Determine if current time is during the day or night
-    const isDayTime = (hour > dayStartHour || (hour === dayStartHour && minute >= dayStartMinute)) &&
-                      (hour < nightStartHour || (hour === nightStartHour && minute < nightStartMinute));
-
-    if (isDayTime) {
-        if (!bodyClass.contains('day')) {
-            bodyClass.add('day');
-            bodyClass.remove('night');
-            sectionClass.add('day');
-            sectionClass.remove('night');
-        }
-    } else {
-        if (!bodyClass.contains('night')) {
-            bodyClass.add('night');
-            bodyClass.remove('day');
-            sectionClass.add('night');
-            sectionClass.remove('day');
-        }
-    }
+    document.documentElement.classList.toggle('night', isDayTime);
+    document.documentElement.classList.toggle('day', !isDayTime);
 }
 
-// Call updateTheme initially and then every minute to ensure the theme updates promptly.
 updateTheme();
-setInterval(updateTheme, 60000); // Checks every minute
+setInterval(updateTheme, 60000);
 
 function preloadImages() {
-  const images = ['../assets/images/logo-dark.png', '../assets/images/logo-light.png'];
-    images.forEach(image => {
+    const images = [
+        'assets/images/logo-dark.png',
+        'assets/images/logo-light.png'
+    ];
+
+    images.forEach((image) => {
         const img = new Image();
         img.src = image;
     });
 }
 
-preloadImages(); // Preload images to avoid flickering during the first switch
+preloadImages();
